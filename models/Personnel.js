42deeef6a87fc3d1df86cf52296b81d3a0c11057
HuynhTonyy <107403128+HuynhTonyy@users.noreply.github.com>
2026-04-05 08:49:04 +0700

    Update Personnel.js

--- models/Personnel.js
+++ models/Personnel.js
@@ -6,84 +6,87 @@ const PersonnelSchema = new mongoose.Schema(
       birthName: { type: String, required: true, trim: true },
       commonName: { type: String, trim: true },
       birthDate: { type: Date, required: true },
-      idNumber: {type: String, required: true, unique: true, trim: true},
-      issueDate: Date,
-      birthPlace: String,
-      hometown: String,
-      ethnicity: String,
-      religion: String,
-      phone: String
+      idNumber: { type: String, required: true, trim: true },
+      issueDate: { type: Date },
+      birthPlace: {type: String},
+      hometown: {type: String},
+      ethnicity: {type: String},
+      religion: {type: String},
+      phone: {
+        type: String,
+        match: /^[0-9]{9,11}$/
+      }
     },
 
     residence: {
-      familyAddress: String,
-      currentAddress: String
+      familyAddress: {type: String},
+      currentAddress: {type: String}
     },
 
     background: {
-      familyComponent: String,
-      selfComponent: String,
-      birthOrder: Number,
-      parentsTotalChildren: Number,
-      selfTotalChildren: Number
+      familyComponent: {type: String},
+      selfComponent: {type: String},
+      birthOrder: { type: Number },
+      parentsTotalChildren: { type: Number },
+      selfTotalChildren: { type: Number }
     },
 
     education: {
-      educationLevel: String,    
-      professionalLevel: String,
-      graduationYear: Number,
-      major: String,
-      foreignLanguage: String
+      educationLevel: {type: String},    
+      professionalLevel: {type: String},
+      graduationYear: { type: Number },
+      major: {type: String},
+      foreignLanguage: {type: String}
     },
 
     partyInfo: {
-      partyJoinDate: Date,
-      officialDate: Date,
-      youthUnionJoinDate: Date
+      partyJoinDate: { type: Date },
+      officialDate: { type: Date },
+      youthUnionJoinDate: { type: Date }
     },
 
     career: {
       job: String,
-      salaryGrade: String,
-      salaryLevel: String,
-      workplace: String,
-      reward: String,
-      discipline: String
+      salaryGrade: {type: String},
+      salaryLevel: {type: String},
+      workplace: {type: String},
+      reward: {type: String},
+      discipline: {type: String}
     },
 
     family: {
       father: {
-        name: String,
-        birthYear: Number,
-        job: String,
+        name: {type: String},
+        birthYear: { type: Number },
+        job: {type: String},
         status: {
           type: String,
           enum: ["Sống", "Chết", "Không rõ"],
           default: "Sống"
         },
-        before1975: String,
-        after1975: String
+        before1975: {type: String},
+        after1975: {type: String}
       },
 
       mother: {
-        name: String,
-        birthYear: Number,
-        job: String,
-        hometown: String,
-        component: String,
+        name: {type: String},
+        birthYear: { type: Number },
+        job: {type: String},
+        hometown: {type: String},
+        component: {type: String},
         status: {
           type: String,
           enum: ["Sống", "Chết", "Không rõ"],
           default: "Sống"
         },
-        before1975: String,
-        after1975: String
+        before1975: {type: String},
+        after1975: {type: String}
       },
 
       spouse: {
-        name: String,
-        birthYear: Number,
-        job: String
+        name: {type: String},
+        birthYear: { type: Number },
+        job: {type: String}
       },
 
       siblings: [
@@ -93,21 +96,26 @@ const PersonnelSchema = new mongoose.Schema(
             enum: ["Anh trai", "Chị gái", "Em trai","Em gái"],
             default: "Anh trai"
             },
-          name: String,
-          birthYear: Number,
-          address: String
+          name: {type: String},
+          birthYear: { type: Number },
+          address: {type: String}
         }
       ]
     },
 
     educationHistory: [
       {
-        period: String,
-        description: String
+        period: {type: String},
+        description: {type: String}
       }
     ],
 
-    localComment: String
+    localComment: {type: String},
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    }
   },
   {
     timestamps: true
